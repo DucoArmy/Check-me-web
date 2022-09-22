f59b96e02ea7ec3e0e5f5f65f5b8f3562e7f54c4
unknown <rto@dgsw.hs.kr>
2022-09-22 20:30:32 +0900

    ..

--- src/component/desks/SmallTable.jsx
+++ src/component/desks/SmallTable.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components"
-const Main = styled.div`
-    width: 400px;
-    height: 400px;
-    border-radius: 50%;
-    background-color: #dadada
+const SmallTable = styled.div`
+    width: 56px;
+    height: 103px;
+    border-radius: 10%;
+    background-color: #dadada;
+    box-shadow: 1px 3px 1px 1px #00000025;
 `
 export default () => {
     return (
-        <Main></Main>
+        <SmallTable></SmallTable>
     )
 }
